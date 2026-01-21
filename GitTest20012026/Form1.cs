e21d5f933c8c4a308ec1cec9a7588372b0c2a2f4
diana-stecka <diana.stecka@gmail.com>
2026-01-21 12:08:28 +0200

    Pievienota poga

--- GitTest20012026/Form1.cs
+++ GitTest20012026/Form1.cs
@@ -16,5 +16,10 @@ namespace GitTest20012026
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
